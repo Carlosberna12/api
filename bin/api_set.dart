aab7f74c3f9775459754f8658b24fef40ff1a2c0
Arath <carlosberna74@gmail.com>
2021-01-25 19:12:09 -0600

    Commit inicio

---
+++ bin/api_set.dart
@@ -0,0 +1,18 @@
+import 'persona.dart';
+import 'triangulo.dart';
+import 'persona.dart';
+
+void main(List<String> arguments) {
+  /* Triangulo triangulo = new Triangulo();
+
+  triangulo.base = 7.5;
+  triangulo.altura = 23.5;
+
+  print("El area del triangulo es: ${triangulo.area}");
+  */
+  Persona p = new Persona();
+
+  p.nombre = 'Arath';
+
+  print('El nombre es: ${p.nombrePersona}');
+}
---
+++ test/api_set_test.dart
@@ -0,0 +1,8 @@
+import 'package:api_set/api_set.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
